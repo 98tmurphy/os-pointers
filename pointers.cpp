d43f4113185a4137ae1c83c2ce6e21a607cdae31
98tmurphy <53626316+98tmurphy@users.noreply.github.com>
2021-02-11 18:47:41 -0600

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,7 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <cmath>
 
 typedef struct Student {
     int id;
@@ -14,15 +15,45 @@ int promptInt(std::string message, int min, int max);
 double promptDouble(std::string message, double min, double max);
 void calculateStudentAverage(void *object, double *avg);
 
+using namespace std;
 int main(int argc, char **argv)
 {
     Student student;
     double average;
+    student.f_name = new char[128];
+    student.l_name = new char[128];
 
     // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("PLEASE ENTER THE STUDENT'S ID: ",0,999999999);
 
-    // Call `CalculateStudentAverage(???, ???)`
+    //get students name
+    std::cout << "PLEASE ENTER THE STUDENT'S FIRST NAME: ";
+    cin >> student.f_name;
+    
+    std::cout << "PLEASE ENTER THE STUDENTS LAST NAME: ";
+    cin >> student.l_name;
+
+    //get number of assignments
+    student.n_assignments = promptInt("PLEASE ENTER THE NUMBER OF ASSIGNMENTS: ", 1, 999999999);
+
+    student.grades = new double[student.n_assignments];
+    int i;
+    average = 0.0;
+
+    //adding 
+    for(i = 0; i < student.n_assignments; i++){
+        std::cout << "PLEASE ENTER GRADE FOR ASSIGNMENT " << i + 1 << ": ";
+        student.grades[i] = promptDouble("", 0, 100.00);
+        average = average + student.grades[i];
+
+    }
+
+    // Call `CalculateStudentAverage(???, ???)`sss
     // Output `average`
+    calculateStudentAverage(&student.n_assignments, &average);
+    
+    std::cout << "STUDENT: " << student.f_name << " " << student.l_name << " [" << student.id << "]" << std::endl;
+    std::cout << "AVERAGE GRADE: " << average;
 
     return 0;
 }
@@ -35,6 +66,13 @@ int main(int argc, char **argv)
 int promptInt(std::string message, int min, int max)
 {
     // Code to prompt user for an int
+
+    int outputINT;
+
+    cout << message;
+    cin >> outputINT;
+
+    return outputINT;
 }
 
 /*
@@ -45,6 +83,12 @@ int promptInt(std::string message, int min, int max)
 double promptDouble(std::string message, double min, double max)
 {
     // Code to prompt user for a double
+    double outputDouble;
+
+    cout << message;
+    cin >> outputDouble;
+
+    return outputDouble;
 }
 
 /*
@@ -54,4 +98,8 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    int denominator = *(int *)object;
+
+    *avg = *avg / denominator;
+    *avg = round(*avg * 10)/10;
 }
